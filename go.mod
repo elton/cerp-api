67ac59d77e162d30abab79432c87776b208b651b
Elton Zheng <elton.zheng.sh@gmail.com>
2021-02-15 17:01:21 +0800

    Add a startup banner for the project.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/elton/cerp-api
 go 1.15
 
 require (
+	github.com/dimiro1/banner v1.1.0
 	github.com/gofiber/fiber/v2 v2.5.0
 	github.com/joho/godotenv v1.3.0
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	_ "github.com/dimiro1/banner/autoload"
+
 	"github.com/elton/cerp-api/api/controllers"
 	"github.com/elton/cerp-api/config"
 )
 
 func main() {
 	var server = controllers.Server{}
-
 	server.Run(config.Config("SERVER_PORT"))
 }
